dc23552ddffc88643873b960cdf73111697e7b6a
Tony Lea <tony.lea@vanquisbank.co.uk>
2023-03-22 10:24:46 +0000

    feat: add getarticletitle to read title from file

---
+++ src/actions/get-article-title.ts
@@ -0,0 +1,15 @@
+import fs from "fs/promises";
+
+export async function getArticleTitle(filepath: string): Promise<string> {
+  let title: string = "";
+
+  const fileContent: string = await fs.readFile(filepath, "utf-8");
+
+  for (const line of fileContent.split("\n")) {
+    const trimmedLine = line.trim();
+    if (trimmedLine.startsWith("#")) {
+      title = trimmedLine.substring(1).trim();
+    }
+  }
+  return title;
+}
