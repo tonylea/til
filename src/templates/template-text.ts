eaf0c06a48b337ab48d81eacca6f80b746412688
Tony Lea <tony.lea@vanquisbank.co.uk>
2023-03-22 10:26:44 +0000

    feat: add template-text.ts with template text for til repo

---
+++ src/templates/template-text.ts
@@ -0,0 +1,36 @@
+const header: string = `# TIL: Today I Learned
+
+This is a collection of small write-ups of things I've learned in my role as a Cloud Engineer and
+general enthusiast of devops.
+
+I got the idea from many, many repos in GitHub doing the same thing. There are too many list.
+Seriously, just search for 'TIL' and you'll see.`;
+
+const footer: string = `## How to Use
+
+Steps to follow:
+
+1. Create directories for specific topics. For example if you learned something new in JavaScript,
+   create a directory for the same.
+
+2. Inside those directories create a [\`Markdown\`](https://www.markdownguide.org/basic-syntax/)
+	 file with your title for example \`Variables-in-JavaScript.md\`,
+	 \`Create_React_App.md\` etc. Make sure that the markdown file has a title.
+	 Spaces in titles are _not_ recommended since different services render
+	 markdown differently.
+
+3. Every Markdown TIL file should start with a \`#\` i.e h1 heading.
+
+4. Once satisfied push your changes. The pipeline will update the \`README\` file automagically.
+
+
+## About
+
+This is the bit where I suppose I should talk about myself. May be I'll get round to updating this
+at some point.
+
+## See Also
+
+Nothing to see here. Move along now, move along.`;
+
+export { header, footer };
